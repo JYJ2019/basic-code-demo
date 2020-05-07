fb47f34c3b23b3ff648491d8b2931e724895a9cf
JYJ <1036470143@qq.com>
2020-05-07 21:14:34 +0800

    5.7

--- day06-code/src/cn/itcast/day06/demo02/Demo01PhoneOne.java
+++ day06-code/src/cn/itcast/day06/demo02/Demo01PhoneOne.java
@@ -2,6 +2,8 @@ package cn.itcast.day06.demo02;
 
 public class Demo01PhoneOne {
     public static void main(String[] args) {
+        //根据Phone类,创建一个名为one的对象
+        //格式:类名称 对象名 = new 类名称();
         Phone one = new Phone();
         System.out.println(one.brand);//null
         System.out.println(one.price);//0.0
@@ -12,9 +14,9 @@ public class Demo01PhoneOne {
         one.price = 8766.92;
         one.color = "red";
 
-        System.out.println(one.brand);//null
-        System.out.println(one.price);//0.0
-        System.out.println(one.color);//null
+        System.out.println(one.brand);//苹果
+        System.out.println(one.price);//8766.92
+        System.out.println(one.color);//red
         System.out.println("======================");
 
         one.call("乔布斯");
